ac953a01ef6c695f22af98fa7ef5d07c564fdc46
Aleksandar Trposki <aleksandartrposki@gmail.com>
2016-02-26 14:21:53 +0100

    Working version. Added purify number funct. First N funct.

--- Numbers.php
+++ Numbers.php
@@ -1,261 +1,288 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Ace
+ * Date: 26.02.2016
+ * Time: 11:32
+ */
+
 
 class DoubleDigitsName {
 
-  public $Male;
-  public $Female;
+    public $Male;
+    public $Female;
 
-  public function __construct($male,$female) {
-    $this->Male = $male;
-	if(empty($female)){
-		$this->Female=$male;
-	}else{
-		$this->Female=$female;
-	}
-  }
+    public function __construct($male,$female) {
+        $this->Male = $male;
+        if(empty($female)){
+            $this->Female=$male;
+        }else{
+            $this->Female=$female;
+        }
+    }
 }
 
 
 
 class TriadName {
 
-  public $Singular;
-  public $Plural;
-  public $IsMasculin;
+    public $Singular;
+    public $Plural;
+    public $IsMasculin;
 
-  public function __construct($sing,$plr,$ismsc) {
-    $this->Singular = $sing;
-	$this->Plural=$plr;
-	$this->IsMasculin = $ismsc;
-  }
+    public function __construct($sing,$plr,$ismsc) {
+        $this->Singular = $sing;
+        $this->Plural=$plr;
+        $this->IsMasculin = $ismsc;
+    }
 }
 
 class Chunck{
-	public $Hundrets;
-	public $Decades;
-	public $Ones;
-	public $DTriad;
-	public $DecadessMap;
-	public $OnessMap;
-	public $SpecialsMap;
-	public $HundretssMap;
-	
-	public function __construct($digits,$triad,$hundretsMap,$decadesMap,$onesMap,$specialDoubleDigitsMap)
-	{
-		$this->DecadessMap=$decadesMap;
-		$this->OnessMap=$onesMap;
-		$this->HundretssMap=$hundretsMap;
-		$this->SpecialsMap=$specialDoubleDigitsMap;
-		$this->DTriad=$triad;
-		$digits = array_reverse($digits);
-		list($this->Ones,$this->Decades,$this->Hundrets) = $digits;
-	}
-	
-	public function PrintDigits(){
-		print $this->Hundrets . $this->Decades . $this->Ones;
-	}
-	
-	public function CalculateName()
-	{
-		$hundretsName = $this->CalculateHundretsName();
-		$decadesAndOnes =$this->CalculateDecadesAndOnesName();
-		$triadeName = $this->CalculateTriadName();
-		$res = $hundretsName . "  " . $decadesAndOnes . " " . $triadeName;
-		$res = trim ($res);
-		$res = ltrim($res,"и");
-		$res = str_replace("  ", " ", $res);
-		return $res;
-	}
-	
-	public function CalculateTriadName()
-	{
-		#000
-		if((!isset($this->Ones)||$this->Ones=="0") && (!isset($this->Decades)||$this->Decades=="0") && (!isset($this->Hundrets)||$this->Hundrets=="0"))
-		{
-			return NULL;
-		}
-		
-		#singular
-		if($this->Ones === "1"){
-			return $this->DTriad->Singular;
-		}
-		
-		#plural
-		return $this->DTriad->Plural;
-	}
+    public $Hundrets;
+    public $Decades;
+    public $Ones;
+    public $DTriad;
+    public $DecadessMap;
+    public $OnessMap;
+    public $SpecialsMap;
+    public $HundretssMap;
 
-	public function CalculateHundretsName()
-	{
-		if(isset($this->Hundrets))
-		{
-			return $this->HundretssMap[$this->Hundrets];
-		}
-		
-		return NULL;
-	}
-	
-	public function CalculateDecadesAndOnesName()
-	{
-		$decadesAndOnesString=NULL;
-		if(array_key_exists ($this->Decades . $this->Ones, $this->SpecialsMap ))
-		{
-			#special case, no need for further calculations;
-			$ddName = $this->SpecialsMap[$this->Decades . $this->Ones];
-			if($this->DTriad->IsMasculin){
-				return  $ddName->Male;
-			}else{
-				return  $ddName->Female;
-			}
-		}
-		
-		$onesName = $this->OnessMap[$this->Ones];
-		if($this->DTriad->IsMasculin){
-			$onesNameString = $onesName->Male;
-		}else{
-			$onesNameString=$onesName->Female;	
-		}
-		
-		if(isset($this->Decades))
-		{
-			$decadesNameString=$this->DecadessMap[$this->Decades];
-		}
-		
-		$decadesAndOnesString = $decadesNameString . " ". $onesNameString;
-		
-		return $decadesAndOnesString;
-	}
-	
+    public function __construct($digits,$triad,$hundretsMap,$decadesMap,$onesMap,$specialDoubleDigitsMap)
+    {
+        $this->DecadessMap=$decadesMap;
+        $this->OnessMap=$onesMap;
+        $this->HundretssMap=$hundretsMap;
+        $this->SpecialsMap=$specialDoubleDigitsMap;
+        $this->DTriad=$triad;
+        $this->Ones="";
+        $this->Decades="";
+        $this->Hundrets="";
+        #$digits = array_reverse($digits);
+        $digitsLength = count($digits);
+        if($digitsLength>=1){
+            $this->Ones=$digits[$digitsLength-1];
+        }
+        if($digitsLength>=2){
+            $this->Decades=$digits[$digitsLength-2];
+        }
+
+        if($digitsLength>=3){
+            $this->Hundrets=$digits[$digitsLength-3];
+        }
+
+    }
+
+    public function PrintDigits(){
+        print $this->Hundrets . $this->Decades . $this->Ones;
+    }
+
+    public function CalculateName()
+    {
+        $hundretsName = $this->CalculateHundretsName();
+        $decadesAndOnes =$this->CalculateDecadesAndOnesName();
+        $triadeName = $this->CalculateTriadName();
+        $res = $hundretsName . "  " . $decadesAndOnes . " " . $triadeName;
+        $res = trim ($res);
+        $res = ltrim($res,"и");
+        $res = str_replace("  ", " ", $res);
+        return $res;
+    }
+
+    public function CalculateTriadName()
+    {
+        #000
+        if((!isset($this->Ones)||$this->Ones=="0") && (!isset($this->Decades)||$this->Decades=="0") && (!isset($this->Hundrets)||$this->Hundrets=="0"))
+        {
+            return NULL;
+        }
+
+        #singular
+        if($this->Ones == "1" && $this->Decades!="1"){
+            return $this->DTriad->Singular;
+        }
+
+        #plural
+        return $this->DTriad->Plural;
+    }
+
+    public function CalculateHundretsName()
+    {
+        if(isset($this->Hundrets))
+        {
+            return $this->HundretssMap[$this->Hundrets];
+        }
+
+        return NULL;
+    }
+
+    public function CalculateDecadesAndOnesName()
+    {
+        $decadesAndOnesString=NULL;
+        if(array_key_exists ($this->Decades . $this->Ones, $this->SpecialsMap ))
+        {
+            #special case, no need for further calculations;
+            $ddName = $this->SpecialsMap[$this->Decades . $this->Ones];
+            if($this->DTriad->IsMasculin){
+                return  $ddName->Male;
+            }else{
+                return  $ddName->Female;
+            }
+        }
+
+        $onesName = $this->OnessMap[$this->Ones];
+        if($this->DTriad->IsMasculin){
+            $onesNameString = $onesName->Male;
+        }else{
+            $onesNameString=$onesName->Female;
+        }
+
+        $decadesNameString="";
+        if(isset($this->Decades))
+        {
+            $decadesNameString=$this->DecadessMap[$this->Decades];
+        }
+
+        $decadesAndOnesString = $decadesNameString . " ". $onesNameString;
+
+        return $decadesAndOnesString;
+    }
+
 }
 
 $SpecialDoubleDigitNames=[
-""=>new DoubleDigitsName("",""),
-"00"=>new DoubleDigitsName("",""),
-"01"=>new DoubleDigitsName("и еден","и еднa"),
-"02"=>new DoubleDigitsName("и двa","и две"),
-"03"=>new DoubleDigitsName("и три",""),
-"04"=>new DoubleDigitsName("и четири",""),
-"05"=>new DoubleDigitsName("и пет",""),
-"06"=>new DoubleDigitsName("и шест",""),
-"07"=>new DoubleDigitsName("и седум",""),
-"08"=>new DoubleDigitsName("и осум",""),
-"09"=>new DoubleDigitsName("и девет",""),
-"10"=>new DoubleDigitsName("дест",""),
-"11"=>new DoubleDigitsName("единаесет",""),
-"12"=>new DoubleDigitsName("дванаесет",""),
-"13"=>new DoubleDigitsName("тринаесет",""),
-"14"=>new DoubleDigitsName("четиринаесет",""),
-"15"=>new DoubleDigitsName("петнаесет",""),
-"16"=>new DoubleDigitsName("шестнаесет",""),
-"17"=>new DoubleDigitsName("седумнаесет",""),
-"18"=>new DoubleDigitsName("осумнаесет",""),
-"19"=>new DoubleDigitsName("деветнаесет","")
+    ""=>new DoubleDigitsName("",""),
+    "00"=>new DoubleDigitsName("",""),
+    "01"=>new DoubleDigitsName("и еден","и еднa"),
+    "02"=>new DoubleDigitsName("и двa","и две"),
+    "03"=>new DoubleDigitsName("и три",""),
+    "04"=>new DoubleDigitsName("и четири",""),
+    "05"=>new DoubleDigitsName("и пет",""),
+    "06"=>new DoubleDigitsName("и шест",""),
+    "07"=>new DoubleDigitsName("и седум",""),
+    "08"=>new DoubleDigitsName("и осум",""),
+    "09"=>new DoubleDigitsName("и девет",""),
+    "10"=>new DoubleDigitsName("и десет",""),
+    "11"=>new DoubleDigitsName("и единаесет",""),
+    "12"=>new DoubleDigitsName("и дванаесет",""),
+    "13"=>new DoubleDigitsName("и тринаесет",""),
+    "14"=>new DoubleDigitsName("и четиринаесет",""),
+    "15"=>new DoubleDigitsName("и петнаесет",""),
+    "16"=>new DoubleDigitsName("и шестнаесет",""),
+    "17"=>new DoubleDigitsName("и седумнаесет",""),
+    "18"=>new DoubleDigitsName("и осумнаесет",""),
+    "19"=>new DoubleDigitsName("и деветнаесет","")
 ];
 
 $FirstDecadeDigits = [
-""=>new DoubleDigitsName("",""),
-"0"=>new DoubleDigitsName("",""),
-"1"=>new DoubleDigitsName("и еден","и еднa"),
-"2"=>new DoubleDigitsName("и двa","и две"),
-"3"=>new DoubleDigitsName("и три",""),
-"4"=>new DoubleDigitsName("и четири",""),
-"5"=>new DoubleDigitsName("и пет",""),
-"6"=>new DoubleDigitsName("и шест",""),
-"7"=>new DoubleDigitsName("и седум",""),
-"8"=>new DoubleDigitsName("и осум",""),
-"9"=>new DoubleDigitsName("и девет","")
+    ""=>new DoubleDigitsName("",""),
+    "0"=>new DoubleDigitsName("",""),
+    "1"=>new DoubleDigitsName("и еден","и еднa"),
+    "2"=>new DoubleDigitsName("и двa","и две"),
+    "3"=>new DoubleDigitsName("и три",""),
+    "4"=>new DoubleDigitsName("и четири",""),
+    "5"=>new DoubleDigitsName("и пет",""),
+    "6"=>new DoubleDigitsName("и шест",""),
+    "7"=>new DoubleDigitsName("и седум",""),
+    "8"=>new DoubleDigitsName("и осум",""),
+    "9"=>new DoubleDigitsName("и девет","")
 ];
 
 $DecadesNames=[
-"2"=>"дваесет",
-"3"=>"триесет",
-"4"=>"четириесет",
-"5"=>"педесет",
-"6"=>"шеесет",
-"7"=>"седумдесет",
-"8"=>"осумдесет",
-"9"=>"деведесет"
+    ""=>"",
+    "2"=>"дваесет",
+    "3"=>"триесет",
+    "4"=>"четириесет",
+    "5"=>"педесет",
+    "6"=>"шеесет",
+    "7"=>"седумдесет",
+    "8"=>"осумдесет",
+    "9"=>"деведесет"
 ];
 
 
 $TriadeNamesMap =[
-	0=>new TriadName("","",true),
-	1=>new TriadName("илјада","илјади",false),
-	2=>new TriadName("милион","милиони",true),
+    0=>new TriadName("","",true),
+    1=>new TriadName("илјада","илјади",false),
+    2=>new TriadName("милион","милиони",true),
 ];
 
 $hundretsNames=[
-	"0"=>"",
-	"1"=>"сто",
-	"2"=>"двесте",
-	"3"=>"триста",
-	"4"=>"четиристотини",
-	"5"=>"петстотини",
-	"6"=>"шестотини",
-	"7"=>"седумстотини",
-	"8"=>"осумстотини",
-	"9"=>"деветстотини"
+    ""=>"",
+    "0"=>"",
+    "1"=>"сто",
+    "2"=>"двесте",
+    "3"=>"триста",
+    "4"=>"четиристотини",
+    "5"=>"петстотини",
+    "6"=>"шестотини",
+    "7"=>"седумстотини",
+    "8"=>"осумстотини",
+    "9"=>"деветстотини"
 ];
 
-function GetNumberAsString ($number,$triadeNamesMap,$DecadesNames,$FirstDecadeDigits,$SpecialDoubleDigitNames )
-{
-	
-	$numberAsArray = str_split($number,1);
-	$reversedNumArr = array_reverse($numberAsArray);
-	$reversedNumString = implode ("",$reversedNumArr );
-	$curTriadeIndex=0;
-	$chunks = array();
-	while($reversedNumString ){
-		$splitBy3 = str_split($reversedNumString ,3);
-		$triadeString = $splitBy3[0];
-		$triadeArr = array_reverse(str_split($triadeString,1));
-		$reversedNumString = ltrim($reversedNumString,$triadeString);
-		
-		$triade = $triadeNamesMap[$curTriadeIndex];
-		$chunk = new Chunck($triadeArr,$triade,$hundretsNames,$DecadesNames,$FirstDecadeDigits,$SpecialDoubleDigitNames);
-		array_push($chunks,$chunk);
-		$curTriadeIndex++;
-	}
-	
-	$chunks= array_reverse($chunks);
-	$res="";
-	$i=0;
-	$chuncksLength = count($chunks);
-	for($i=0;$i<$chuncksLength-1;$i++)
-	{
-		$res = $res . " " . $chunk->CalculateName();
-	}
-	
-	$lastChunkName = $chunks[$chuncksLength-1]->CalculateName();
-	if(trim($res)!=""){
-		$res = $res . " и " . $lastChunkName;
-	}else{
-		$res = $res . "  " . $lastChunkName;
-	}
-	
-	return str_replace("  ", " ", $res);
+function PrintArr($arr){
+    foreach($arr as $a)
+    {
+        print $a . " | ";
+    }
+    echo "<br/>";
 }
 
-$tryChunk = new Chunck(array(1,2,3),new TriadName("илјада","илјади",false),$hundretsNames,$DecadesNames,$FirstDecadeDigits,$SpecialDoubleDigitNames);
-echo $tryChunk->DTriad->Female;
-echo $tryChunk->Ones;
-echo $tryChunk->CalculateName() . "\xA";
+function FirstN($arr,$n){
+    $i=0;
+    $arrLen = count($arr);
+    $res = array();
+    for($i=0; $i<$arrLen && $i<$n; $i++)
+    {
+       array_push($res,$arr[$i]);
+    }
+    return $res;
+}
 
-$tryChunk = new Chunck(array(1,2,2),new TriadName("илјада","илјади",false),$hundretsNames,$DecadesNames,$FirstDecadeDigits,$SpecialDoubleDigitNames);
-echo $tryChunk->CalculateName() . "\xA";
-$tryChunk = new Chunck(array(1,1,2),new TriadName("илјада","илјади",false),$hundretsNames,$DecadesNames,$FirstDecadeDigits,$SpecialDoubleDigitNames);
-echo $tryChunk->CalculateName() . "\xA";
-$tryChunk = new Chunck(array(1,0,3),new TriadName("илјада","илјади",false),$hundretsNames,$DecadesNames,$FirstDecadeDigits,$SpecialDoubleDigitNames);
-echo $tryChunk->CalculateName() . "\xA";
-$tryChunk = new Chunck(array(0,0,3),new TriadName("илјада","илјади",false),$hundretsNames,$DecadesNames,$FirstDecadeDigits,$SpecialDoubleDigitNames);
-echo $tryChunk->CalculateName() . "\xA";
+function PurifyNumber($inpureNumber)
+{
+    $inpureNumber = str_split($inpureNumber,1);
+    $pureNumber = array();
+    foreach($inpureNumber as $digit){
+        if(is_numeric ($digit)){
+            array_push($pureNumber,$digit);
+        }
+    }
+    return implode("",$pureNumber);
+}
 
-$tryChunk = new Chunck(array(3),new TriadName("илјада","илјади",false),$hundretsNames,$DecadesNames,$FirstDecadeDigits,$SpecialDoubleDigitNames);
-echo $tryChunk->CalculateName() . "\xA";
-$tryChunk = new Chunck(array(1,2),new TriadName("илјада","илјади",false),$hundretsNames,$DecadesNames,$FirstDecadeDigits,$SpecialDoubleDigitNames);
-echo $tryChunk->CalculateName() . "\xA";
 
-$tryChunk = new Chunck(array(1,0,0),new TriadName("илјада","илјади",false),$hundretsNames,$DecadesNames,$FirstDecadeDigits,$SpecialDoubleDigitNames);
-echo $tryChunk->CalculateName() . "\xA";
 
+$numberStr = PurifyNumber("10,000");
+$reversedNumArr = str_split($numberStr,1);
+$reversedNumArr = array_reverse($reversedNumArr);
+$triadIndex=0;
+$chunks = array();
+while(count($reversedNumArr)>0){
+    $triChunk = FirstN($reversedNumArr,3);
+    $reversedNumArr = array_slice($reversedNumArr,count($triChunk),count($reversedNumArr));
+    $triChunk = array_reverse($triChunk);
+    $triad = $TriadeNamesMap[$triadIndex];
+    $chunk = new Chunck($triChunk,$triad,$hundretsNames,$DecadesNames,$FirstDecadeDigits,$SpecialDoubleDigitNames);
+    array_push($chunks,$chunk);
+    $triadIndex++;
+}
+
+$chunks=array_reverse($chunks);
+$res = "";
+for($i = 0; $i<count($chunks)-1; $i++){
+    $res = $res . $chunks[$i]->CalculateName() . " ";
+}
+
+$lastChunkName = $chunks[count($chunks)-1]->CalculateName();
+if(trim($res)!=""){
+   $res =   $res . " и " . $lastChunkName;
+}else{
+    $res =  $lastChunkName;
+}
+
+$res = trim($res);
+$res = rtrim($res,"и");
+print str_replace("  "," ",trim($res));
 
 ?>
